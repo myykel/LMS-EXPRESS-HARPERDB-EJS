da54e15de3e83245ba5e9a81bfdb22bcbb514ab9
myykel <ebojimichael@gmail.com>
2023-06-04 17:51:09 +0100

    code refactor

--- app.js
+++ app.js
@@ -18,9 +18,95 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/images",express.static(path.join(__dirname, "images")));
 
 app.get("/",(req, res)=>{
-    res.render("index",{
-        pageTitle: "Home | LMS",
-    })
+
+    db.searchByValue(
+        {
+        operation:"search_by_value",
+        schema:"book",
+        table:"record",
+        searchValue:"*",
+        searchAttribute:"record_id",
+        attributes:["*"]
+
+    },
+    (err, response)=>{
+        if (err) {
+            return res.status(500).json(err);
+        }
+        res.render("index",{
+            pageTitle:"Home | LMS ",
+            records:response.data
+        });
+    }
+    )
+})
+
+//add new book
+app.post("/book/add",(req, res)=>{
+    // Get the user data from the request
+    const {isbn,title,author,description,publisher} = req.body
+
+    // Insert a new record into the database
+    db.insert(
+        {
+          operation:"insert",
+          schema:"book",
+          table:"record",
+          records:[{
+            isbn,title,author,description,publisher
+          }]
+        },
+        (err, response)=>{
+            if (err) {
+                return res.status(500).json(err);
+              }
+        // After creating a new record successfully, redirect back to homepage
+              res.redirect(302, "/");
+        }
+    )
+})
+
+// Updating a record
+app.post("/book/edit",(req, res)=>{
+    const {record_id,isbn,title,author,description,publisher} = req.body
+    db.update(
+        {
+            operation:"update",
+            schema:"book",
+            table:"record",
+            records:[{
+                record_id,isbn,title,author,description,publisher
+              }]
+        },
+        (err, response)=>{
+            if(err){
+                res.status(500).json(err)
+            }
+            // After updating successfully, kindly redirect back to homepage
+            res.redirect(302,"/")
+        }
+    )
+
+})
+
+// Delete action
+app.post("/book/delete/:record_id",(req,res)=>{
+    const {record_id}= req.params
+db.delete(
+    {
+        operation:"delete",
+        schema:"book",
+        table:"record",
+        hashValues:[record_id]
+    },
+
+    (err, response)=>{
+        if(err){
+            return res.status(500).json(err)
+        }
+        res.redirect(302,"/")
+    }
+)
 })
 
 module.exports = app
